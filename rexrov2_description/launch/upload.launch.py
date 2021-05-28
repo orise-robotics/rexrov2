eae97e505060ffd1979b5c94a0f547679a2b8d5e
João Victor Torres Borges <joao.victor.tb@gmail.com>
2021-05-28 09:50:46 -0300

    fix: spawn with Command and PathJoinSubstitution

--- rexrov2_description/launch/upload.launch.py
+++ rexrov2_description/launch/upload.launch.py
@@ -15,12 +15,14 @@
 
 import os
 
-from ament_index_python.packages import get_package_share_directory
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
+from launch_ros.substitutions import ExecutableInPackage
 from launch.actions import ExecuteProcess
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration
+from launch.substitutions import PathJoinSubstitution
+from launch.substitutions import Command
 import xacro
 
 def generate_launch_description():
@@ -28,8 +30,8 @@ def generate_launch_description():
     x = LaunchConfiguration('x', default='0.0')
     y = LaunchConfiguration('y', default='0.0')
     z = LaunchConfiguration('z', default='1.0')
-    urdf_file = os.path.join(get_package_share_directory('rexrov2_description'), 'robots/rexrov2_default.xacro')
-    robot = xacro.process(urdf_file)
+    urdf_file = PathJoinSubstitution([FindPackageShare('rexrov2_description'), 'robots/rexrov2_default.xacro'])
+    #robot = xacro.process(urdf_file)
 
     gazebo = ExecuteProcess(
           cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_factory.so'],
@@ -42,7 +44,9 @@ def generate_launch_description():
             output='screen',
             parameters=[{
                 'robot_description':
-                robot
+                Command([
+                    ExecutableInPackage(package='xacro', executable='xacro'), ' ', urdf_file
+                ])
             }])
 
     spawn_entity = Node(
